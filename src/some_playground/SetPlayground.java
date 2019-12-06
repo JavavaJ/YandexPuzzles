111b1b9b897b6c8e3c0d5e8bf1fa23a45b1a405a
alex <alex@MBP-alex.inex.local>
2019-12-06 15:05:36 +0300

    Some other ways to create a set

--- src/some_playground/SetPlayground.java
+++ src/some_playground/SetPlayground.java
@@ -3,16 +3,27 @@ package some_playground;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class SetPlayground {
 
     public static void main(String[] args) {
+
         int [] arr = new int[] {1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5};
         List<Integer> arrIntegers = Arrays.stream(arr)
                 .boxed()
                 .collect(Collectors.toList());
         HashSet<Integer> integerSet = new HashSet<>(arrIntegers);
+
+        System.out.println(integerSet);
+
+        Set<Integer> intSet = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.toSet());
+
+        System.out.println(intSet);
+
     }
 
 }
