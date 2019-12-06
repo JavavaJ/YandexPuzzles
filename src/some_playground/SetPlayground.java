cfd95efb2dbb64a6894273643952fdd4ac1abf31
alex <alex@MBP-alex.inex.local>
2019-12-06 14:57:46 +0300

    Create set from array of integers

--- src/some_playground/SetPlayground.java
+++ src/some_playground/SetPlayground.java
@@ -1,7 +1,18 @@
 package some_playground;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SetPlayground {
 
-
+    public static void main(String[] args) {
+        int [] arr = new int[] {1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5};
+        List<Integer> arrIntegers = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.toList());
+        HashSet<Integer> integerSet = new HashSet<>(arrIntegers);
+    }
 
 }
