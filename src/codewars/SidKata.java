2be8dd54147798b67a99f4e344c28338f7440d1d
alex <fedotenko.aleksey@gmail.com>
2019-12-07 15:58:05 +0300

    SidKata class commit

--- src/codewars/SidKata.java
+++ src/codewars/SidKata.java
@@ -20,6 +20,19 @@ public class SidKata {
         System.out.println(howMuchILoveYou(1));
         System.out.println(howMuchILoveYou(2));
         System.out.println(howMuchILoveYou(6));
+        System.out.println(oneLiner(1));
+        System.out.println(oneLiner(2));
+        System.out.println(oneLiner(6));
+    }
+
+    public static String oneLiner(int nb_petals) {
+        return Arrays.asList("I love you",
+                "a little",
+                "a lot",
+                "passionately",
+                "madly",
+                "not at all")
+                .get((nb_petals - 1) % 6);
     }
 
 }
