641a944f0eabd5bc716ad30bac1f97aaf0e51c75
alex <fedotenko.aleksey@gmail.com>
2019-12-16 09:36:01 +0300

    Create new puzzle branch

---
+++ src/codewars/MIxedSum.java
@@ -0,0 +1,14 @@
+package codewars;
+
+import java.util.List;
+
+public class MIxedSum {
+
+    /*
+     * Assume input will be only of Integer o String type
+     */
+    public int sum(List<?> mixed) {
+        return -1;
+    }
+
+}
