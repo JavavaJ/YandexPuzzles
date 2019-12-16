3f6f862917d34c86614424d52934af7e00a693d0
alex <fedotenko.aleksey@gmail.com>
2019-12-16 15:55:10 +0300

    addition

--- src/some_playground/Class2.java
+++ src/some_playground/Class2.java
@@ -1,4 +1,15 @@
 package some_playground;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Class2 {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("done");
+
+    }
+
 }
